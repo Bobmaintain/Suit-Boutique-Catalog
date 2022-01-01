7e5353e80c0512b2772623bbf5840f50375e54c6
Dubem JONAH <67968187+Bobmaintain@users.noreply.github.com>
2022-01-01 08:55:04 +0100

    Add files via upload

---
+++ SuitBoutiqueCatalog/pictureGenerator.js
@@ -0,0 +1,6 @@
+function generateSuit() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flexboxContainerImg');
+    image.src = "../pictureGenerator_img/";
+    div.appendChild(image);
+}
